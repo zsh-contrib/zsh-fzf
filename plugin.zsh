e935545d923d14eef55533f9088e35bd4780be59
Svetlin Ralchev <iamralch@users.noreply.github.com>
2025-03-16 10:23:30 +0400

    refactor(plugin.zsh): remove interactive check for zle widgets
    Removed the conditional check for interactive shell before creating and
    binding zle widgets. This simplifies the code and ensures the widgets
    are always available.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- plugin.zsh
+++ plugin.zsh
@@ -8,12 +8,10 @@ source "${0:A:h}/widgets/open.zsh"
 source "${0:A:h}/widgets/tmux.zsh"
 source "${0:A:h}/widgets/zoxide.zsh"
 
-if [[ -o interactive ]]; then
-  # Create the zle widgets
-  zle -N tmux_fzf_switch
-  zle -N tmux_fzf_session
+# Create the zle widgets
+zle -N tmux_fzf_switch
+zle -N tmux_fzf_session
 
-  # Bind the zle widgets
-  bindkey '^[s' tmux_fzf_switch
-  bindkey '^[p' tmux_fzf_session
-fi
+# Bind the zle widgets
+# bindkey '^s' tmux_fzf_switch
+# bindkey '^p' tmux_fzf_session
