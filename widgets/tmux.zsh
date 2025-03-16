d4e6f1fc42f85989fda16797f750f332236bd580
Svetlin Ralchev <iamralch@users.noreply.github.com>
2025-03-16 22:04:42 +0400

    feat(widgets): enhance tmux session selection
    Add additional key bindings to fzf session selection for tmux.
    Now supports 'space' to jump and 'jump' to accept the selection.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- widgets/tmux.zsh
+++ widgets/tmux.zsh
@@ -60,7 +60,7 @@ fzf_tmux_session() {
   local SESSION_NAME
 
   # List sessions, extract their names, and use fzf to select one.
-  SESSION_NAME=$(tmux ls | cut -d: -f1 | fzf --header=' Session' --bind='alt-x:execute(tmux kill-session -t {})+accept')
+  SESSION_NAME=$(tmux ls | cut -d: -f1 | fzf --header=' Session' --bind='alt-x:execute(tmux kill-session -t {})+reload(tmux ls | cut -d: -f1),space:jump,jump:accept')
 
   # If a session is selected, switch to it.
   if [[ -n $SESSION_NAME ]]; then
