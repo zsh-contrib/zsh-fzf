b5602e787e979815c5fa5b9415dafc56d75947f6
Svetlin Ralchev <iamralch@users.noreply.github.com>
2025-03-16 10:19:44 +0400

    fix(zsh): extend FZF_DEFAULT_OPTS and conditionally bind keys
    Extended FZF_DEFAULT_OPTS to include additional options and
    conditionally created and bound zle widgets only in interactive
    shells. This ensures the widgets are not created in non-interactive
    shells, preventing potential errors.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- plugin.zsh
+++ plugin.zsh
@@ -8,10 +8,12 @@ source "${0:A:h}/widgets/open.zsh"
 source "${0:A:h}/widgets/tmux.zsh"
 source "${0:A:h}/widgets/zoxide.zsh"
 
-# Create the zle widgets
-zle -N tmux_fzf_switch
-zle -N tmux_fzf_session
+if [[ -o interactive ]]; then
+  # Create the zle widgets
+  zle -N tmux_fzf_switch
+  zle -N tmux_fzf_session
 
-# Bind the zle widgets
-bindkey '^[s' tmux_fzf_switch
-bindkey '^[p' tmux_fzf_session
+  # Bind the zle widgets
+  bindkey '^[s' tmux_fzf_switch
+  bindkey '^[p' tmux_fzf_session
+fi
