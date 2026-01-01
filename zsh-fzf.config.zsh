3bea6961cc54a1e631125bc393857e90e8e3231d
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-01-01 21:09:06 +0400

    style(fzf): refine default FZF options
    - Simplify FZF default options by removing unnecessary layout and border settings
    - Update prompt icon to a more modern variant

--- zsh-fzf.config.zsh
+++ zsh-fzf.config.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 export FZF_THEME_DIR="${0:A:h}/themes"
-export FZF_DEFAULT_OPTS="--style=minimal --layout reverse-list --highlight-line --prompt='   ' --gutter='' --header-border=sharp --margin=1"
+export FZF_DEFAULT_OPTS="--style=minimal --highlight-line --prompt='󰥭 ' --gutter=''"
 # open file or directory as you wish
 export FZF_CTRL_T_OPTS="--bind 'alt-enter:become(vifm {}),alt-O:become(open {})' --header='  Finder'"
 export FZF_ALT_C_OPTS="--bind 'alt-enter:become(vifm {}),alt-O:become(open {})' --header='  Directories'"
